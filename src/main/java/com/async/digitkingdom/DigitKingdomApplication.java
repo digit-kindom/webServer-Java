e962e00a076e0c71e202232b5a4e5069f93a9819
Evolver257 <2574020134@qq.com>
2024-04-05 21:59:02 +0800

    项目初始化

---
+++ src/main/java/com/async/digitkingdom/DigitKingdomApplication.java
@@ -0,0 +1,13 @@
+package com.async.digitkingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitKingdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitKingdomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/async/digitkingdom/DigitKingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.async.digitkingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitKingdomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
